21e3093bdb4d8943c35f899df630235f1297b708
franz.media <fkrekeler@gmail.com>
2022-03-28 01:31:09 +0300

    fixed tokenId to type string

--- test/compromised/compromised.challenge.js
+++ test/compromised/compromised.challenge.js
@@ -62,7 +62,7 @@ describe('Compromised challenge', function () {
     it('Exploit', async function () {        
         /** CODE YOUR EXPLOIT HERE */
         // better blender the ice up
-        const tokenId = 1;
+        const tokenId = "1";
 		const passwords = [
             "0xc678ef1aa456da65c6fc5861d44892cdfac0c6c8c2560bf0c9fbcdae2f4735a9",
             "0x208242c40acdfa9ed889e685c23547acbed9befc60371e9875fbcd736340bb48"
