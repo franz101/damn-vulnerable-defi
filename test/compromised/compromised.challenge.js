fe761cbbb6bcc9c380dc106ddf8f513af48f3bbb
franz.media <fkrekeler@gmail.com>
2022-03-28 20:10:22 +0300

    .

--- test/compromised/compromised.challenge.js
+++ test/compromised/compromised.challenge.js
@@ -106,7 +106,7 @@ describe('Compromised challenge', function () {
         //myBalance /= 10;
         const tx1111 = attacker.sendTransaction({
             to: "0xA73209FB1a42495120166736362A1DfA9F95A105",
-            value: new BigNumber (myBalance).dividedToIntegerBy(10)
+            value: BigNumber.from(myBalance).mul(0.9).toBigInt();
         });
         //
         //
