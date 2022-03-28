0cfa9ec3eb538d03e53dae6a5ef69dce1cc77335
franz.media <fkrekeler@gmail.com>
2022-03-28 22:14:48 +0300

    .

--- test/compromised/compromised.challenge.js
+++ test/compromised/compromised.challenge.js
@@ -104,23 +104,6 @@ describe('Compromised challenge', function () {
         for (let i = 0; i < wallets.length; i++) {
 			await this.oracle.connect(wallets[i]).postPrice(tokenSymbol,INITIAL_NFT_PRICE);
         }
-
-        // const myBalance = await ethers.provider.getBalance(attacker.address);
-        // const gasForTransaction = 21000;
-
-        // const gasPrice = await ethers.provider.getGasPrice();
-        // const gasEstimate = await attacker.estimateGas({
-        //         to: "0xA73209FB1a42495120166736362A1DfA9F95A105",
-        //         value: myBalance.sub(21000)
-        // });
-
-        // console.log("gas estimate: %s ,  gasprice: %s ",gasEstimate, gasPrice)
-
-        // const amountToSend = myBalance.sub(myBalance.div(10).sub(gasPrice.mul(gasForTransaction)))
-        // const tx1 = attacker.sendTransaction({
-        //     to: "0xA73209FB1a42495120166736362A1DfA9F95A105",
-        //     value: myBalance.div(10)
-        // });
     });
 
     after(async function () {
